05b3695bf486b69e8d4f032ceff346e55d5d54bc
Oliver Foster <oliver.foster@kineo.com>
2015-08-28 13:45:58 +0100

    bugfix: background-image urls
    Background images urls are wrapped with "url()"
    
    This fix removed the "url(" header and ")" footer to enable and img tag to use it as a src attribute

--- jquery.imageready.js
+++ jquery.imageready.js
@@ -3,6 +3,8 @@
 ;(function( $ ) {
 
 	if ($.fn.imageready) return;
+	
+	var stripCSSURL = /url\(([^)]*)\)/g;
 
 	$.fn.imageready = function(callback, options) {
 		//setup options
@@ -22,7 +24,11 @@
 			var $backgroundImageElements = $(getElementsByCSSAttributeName.call(this, "background-image"));
 			$backgroundImageElements.each(function() {
 				var $backgroundImage = $(new Image());
-				$backgroundImage.attr("src", $(this).css("background-image"));
+				var backgroundImageValue = $(this).css("background-image");
+				var matches = stripCSSURL.exec(backgroundImageValue);
+				if (matches === null) return;
+				var url = matches[1];
+				$backgroundImage.attr("src", url);
 				$images.add($backgroundImage);
 			});
 		});
